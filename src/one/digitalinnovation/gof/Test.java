f605210211c3ce4a7bb5e1ca35bdb093bc328fb3
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2023-04-22 20:01:24 -0300

    Laboratorio de Padrões de Projeto Java, Facade / Singleton / Strategy

---
+++ src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,40 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+        holder = SingletonLazyHolder.getInstancia();
+        System.out.println(holder);
+
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Claudio","14801788");
+
+    }
+}
---
+++ src/one/digitalinnovation/gof/strategy/ComportamentoNormal.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.gof.strategy;
+
+public class ComportamentoNormal implements Comportamento{
+
+    @Override
+    public void mover() {
+
+        System.out.print("Movendo-se normalmente");
+
+    }
+}
